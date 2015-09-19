8cda0ad6af7f521704e4c3c6ea219a96eb1e0665
Andy Kearney <akearney.hmc.edu>
2015-09-19 15:58:46 -0700

    Add selecting of dashboard items

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -10,9 +10,22 @@ function Dashboard() {
 
   Dashboard.prototype.handleGridClick = function(event) {
     var $img = $(event.target);
+    var toRemove = -1;
+    for (var i in me.selectedImages_) {
+      var $otherImg = me.selectedImages_[i];
+      if ($img[0] == $otherImg[0]) {
+        toRemove = i;
+        break;
+      }
+    }
+    if (toRemove != -1) {
+      me.selectedImages_.splice(toRemove, 1);
+      $img.removeClass(SELECTED_GRID_IMAGE_CLASS);
+      return;
+    }
     $img.addClass(SELECTED_GRID_IMAGE_CLASS);
     if (me.selectedImages_.length >= NUMBER_SELECTABLE) {
-      var unselect = me.selectedImages_.splice(0,1);
+      var unselect = me.selectedImages_.splice(0,1)[0];
       unselect.removeClass(SELECTED_GRID_IMAGE_CLASS);
     }
     me.selectedImages_.push($img);
