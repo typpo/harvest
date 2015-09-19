f54ef5f8b8a36e379f1656e21fff6998d9a87eec
Ian Webster <ianw_github@ianww.com>
2015-09-19 15:21:55 -0700

    fuick

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
 import os
+#from process_infrablue import ndvi
 
 PIPELINE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+outdir = os.path.join(PIPELINE_DIR, 'images/processed-nvdi/')
+os.system('rm -rf "%s"' % outdir)
+os.system('mkdir "%s"' % outdir)
 
 files = os.listdir(os.path.join(PIPELINE_DIR, 'images/original'))
 for filename in files:
@@ -11,5 +16,7 @@ for filename in files:
 
     print fullpath, '-->', outpath
 
-    cmd = 'infrapix_single -i "%s" -o "%s"' % (fullpath, outpath)
+    #ndvi(fullpath, outpath, vmax=10, vmax=1000)
+
+    cmd = 'infrapix_single -i "%s" -o "%s" --vmin 10 --vmax 1000' % (fullpath, outpath)
     os.system(cmd)
