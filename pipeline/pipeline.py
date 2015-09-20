57196689ed0343012fdf053f9980e8888c8f03f5
Ian Webster <ianw_github@ianww.com>
2015-09-19 19:00:23 -0700

    fix paths

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -5,18 +5,18 @@ import os
 
 PIPELINE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-outdir = os.path.join(PIPELINE_DIR, 'images/processed-nvdi/')
+outdir = os.path.join(PIPELINE_DIR, 'images/processed-ndvi/')
 os.system('rm -rf "%s"' % outdir)
 os.system('mkdir "%s"' % outdir)
 
 files = os.listdir(os.path.join(PIPELINE_DIR, 'images/original'))
 for filename in files:
     fullpath = os.path.join(PIPELINE_DIR, 'images/original/', filename)
-    outpath = os.path.join(PIPELINE_DIR, 'images/processed-nvdi/', filename)
+    outpath = os.path.join(PIPELINE_DIR, 'images/processed-ndvi/', filename)
 
     print fullpath, '-->', outpath
 
     #ndvi(fullpath, outpath, vmax=10, vmax=1000)
 
-    cmd = 'infrapix_single -i "%s" -o "%s" --vmin 10 --vmax 10000' % (fullpath, outpath)
+    0cmd = 'infrapix_single -i "%s" -o "%s" --vmin 10 --vmax 10000' % (fullpath, outpath)
     os.system(cmd)
